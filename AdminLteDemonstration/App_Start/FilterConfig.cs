1a44cb9fa775d297858c1de0a2e390d5df1fbed5
manfered <35757736+manfered@users.noreply.github.com>
2018-08-07 10:49:32 -0600

    Add project files.

---
+++ AdminLteDemonstration/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AdminLteDemonstration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AdminLteDemonstration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminLteDemonstration.Startup))]
+namespace AdminLteDemonstration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
